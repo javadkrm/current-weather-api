ac79b5e74e17dc951376e1229d6cb7b606ec50ee
javad <142673348+javadkrm@users.noreply.github.com>
2023-09-14 19:19:49 +0330

    create base.py

---
+++ base.py
@@ -0,0 +1,13 @@
+from abc import ABC,abstractmethod
+import requests
+
+class weatherapi(ABC):
+    def __init__(self,latitude,longitude,**kwargs):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def get_current_temperature(self):
+        params = {"longitude":self.longitude,"latitude":self.latitude,"current_weather":True}
+        result = requests.get("https://api.open-meteo.com/v1/forecast",params=params)
+        result_jason = result.json()
+        return result_jason["current_weather"]["temperature"]     
